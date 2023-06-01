b63940b58da15ef8e83b15e2a596b98014d30c46
RautMahendrakumar123 <rautmahendrakumar2@gmail.com>
2023-06-01 15:26:20 +0530

    assignment added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
+  const [data,setData]=useState([])
+  const [page,setPage]=useState(1)
+
+  useEffect(()=>{
+    issue(page)
+  },[page])
+
+  const issue = (pageNumberHere)=>{
+    fetch(`https://api.github.com/repositories/1296269/issues?page=${pageNumberHere}&per_page=5`)
+    .then(res=>res.json())
+   .then(data=>{
+    const issue = data.map(issuename=>issuename.title)
+    setData(issue)
+   })
+  }
+
+  console.log(page)
+  
+  const next=()=>{
+    setPage(page +1)
+  }
+  const previous=()=>{
+    if(page===1) return;
+    setPage(page-1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <ul>
+      {
+        data.map((index,key)=>{
+          return <li key={key}>{index}</li>
+        })
+      }
+     </ul>
+     <button className='btn' onClick={previous}>Previous</button>
+     <button className='btn' onClick={next}>Next</button>
     </div>
   );
 }
